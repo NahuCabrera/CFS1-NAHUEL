b073e802d5838101586fd3e0b6ac1a0814c17e08
NahuCabrera <nahucabrera732@gmail.com>
2024-02-29 21:26:34 -0300

    Mi primer commit

---
+++ Clase 1/ejercicio1.ts
@@ -0,0 +1,13 @@
+console.log("PASOS PARA COCINAR UNA TORTA");
+console.log("1)  Comprar los ingredientes");
+console.log("2)  Encender el horno");
+console.log("3)  Tomar un boul");
+console.log("4)  Poner los ingredientes húmedos");
+console.log("5)  Incorporar los ingredientes secos");
+console.log("6)  Batir durante 10 minutos");
+console.log("7)  Enmantercar un recipiente para torta");
+console.log("8)  Colocar la masa en el recipiente");
+console.log("9)  Poner el recipiente en el horno");
+console.log("10) Cocinar durante 50 minutos a fuego medio");
+console.log("11) Retirar y dejar enfriar");
+console.log("12) Disfrutar");
